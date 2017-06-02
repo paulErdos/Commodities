d0068b2630ab7a1e4a955ae56ccdf74434627811
Vincent Steffens <vincesteffens@gmail.com>
2017-06-01 22:26:39 -0700

    Added docstrings and comments.

--- compute.py
+++ compute.py
@@ -7,26 +7,59 @@ import datetime
 import numpy as np
 
 def init():
+    """
+    This function generally verifies that the program is called correctly.
+
+    It verifies:
+    * Proper number of arguments, and
+    * That the date arguments can be split on "-" into three integers, and
+    * That the start date occurs strictly before the end date.
+
+    It does not verify:
+    * That the commodity type given as the final argument makes sense.
+    """
+
+    # Make sure the program is being called correctly. If it isn't, print some
+    # helpful information and exit.
     if len(sys.argv) is not 4:
         print "Usage: $./compute.py start_date end_date commodity_type"
         sys.exit(-1)
 
     # Check that arguments are formatted properly
-    # Check that end date is strictly later than start date
+    try:
+        start_year, start_month, start_day = sys.argv[1].split('-')
+        end_year, end_month, end_day = sys.argv[2].split('-')
 
-    start_year, start_month, start_day = sys.argv[1].split('-')
-    start = datetime.date(int(start_year), int(start_month), int(start_day))
+        start =\
+            datetime.date(int(start_year), int(start_month), int(start_day))
+        end =\
+             datetime.date(int(end_year), int(end_month), int(end_day))
+    except:
+        print "Error: Cannot make sense of date arguments!\n"
+        print "The arguments in question:"
+        print "Argument 1: " + sys.argv[1]
+        print "Argument 2: " + sys.argv[2]
+        print "\nQuitting!"
+        exit(-1)
 
-    end_year, end_month, end_day = sys.argv[2].split('-')
-    end   = datetime.date(int(end_year), int(end_month), int(end_day))
-
+    # Verify that the dates are in order
     if not start < end:
         print "Error: Start date does not precede end date.\n"
         exit(-1)
 
     return {'start': start, 'end': end, 'ctype': sys.argv[3]}
 
+
 def read_and_format(ctype):
+    """
+    This function takes a commodity name and returns its price history.
+
+    The file corresponding to the commodity type is opened, and its contents
+    are read in. Date information is converted to the python datetime.date
+    type, while prices are converted to floats. Tuples of dates and prices are
+    then stored in a list, which is returned.
+    """
+
     table = []
 
     months = {
@@ -44,25 +77,37 @@ def read_and_format(ctype):
         "Dec": 12,
     }
 
+    # Select the specified data file
     filename = 'data_' + ctype + '.csv'
     if os.path.isfile(filename):
         with open(filename) as f:
             # Discard the header line
             f.readline()
-
             for line in f:
+                # Remove the trailing newline character
                 line = line.rstrip()
                 date, price = line.split(',')
 
-                # Convert date
+                # Convert the first column into date objects
                 month, day, year = date.split(' ')
                 month = months[month]
                 date = datetime.date(int(year), month, int(day))
 
-                table.append((date, float(price)))
+                # Reformat the price
+                price = float(price)
+
+                # Store the data in the table
+                table.append((date, price))
     return table
 
 def select(start, end, table):
+    """
+    Returns a list of prices corresponding to the given date range.
+
+    This function also verifies that the given dates are within the bounds of
+    the table.
+    """
+
     # Make sure start and end dates are strictly on the table
     if start < (table[-1])[0]:
         print "Error: Start date before earliest datum."
@@ -75,6 +120,8 @@ def select(start, end, table):
     selection = [x[1] for x in table if x[0] >= start and x[0] <= end]
     return selection
 
+# Print the mean and variance of the price of a commodity over a given date
+# range.
 Parameters = init()
 Table = read_and_format(Parameters['ctype'])
 Selection = select(Parameters['start'], Parameters['end'], Table)
