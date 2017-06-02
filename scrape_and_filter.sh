e0d38a826492a1107924e50de68eba6b6f7f62cd
Vincent Steffens <vincesteffens@gmail.com>
2017-06-01 20:12:14 -0700

    Figured out how to replace newlines in sed! Bash file now produces CSVs!

--- scrape_and_filter.sh
+++ scrape_and_filter.sh
@@ -8,12 +8,17 @@ url_silver='https://www.investing.com/commodities/silver-historical-data'
 curl --user-agent "Mozilla/5.0" $url_gold > page_gold
 curl --user-agent "Mozilla/5.0" $url_silver > page_silver
 
+# Create files to store the data
+echo "Date,Price" > data_gold.csv
+echo "Date,Price" > data_silver.csv
+
 # For all the lines in the file, grab the ones that contain "data-real-value"
 # For all of those, grab the ones that contain "class"
 # For all of those, grab all the ones that don't contain a "%"
-# Then remove the HTML tags and write the results to a file.
-cat page_gold | grep data-real-value | grep class | egrep -v "%" | sed 's:[ ]*<[^>]*>::g' > raw_data_gold
-cat page_silver | grep data-real-value | grep class | egrep -v "%" | sed 's:[ ]*<[^>]*>::g' > raw_data_silver
+# Then remove the HTML tags and commas, replace every other newline with a
+# comma and write the results to a file.
+cat page_gold | grep data-real-value | grep class | egrep -v "%" | sed 's:[ ]*<[^>]*>::g' | sed 's/,//' | sed 'N;s/\n/,/' > data_gold.csv
+cat page_silver | grep data-real-value | grep class | egrep -v "%" | sed 's:[ ]*<[^>]*>::g' | sed 's/,//' | sed 'N;s/\n/,/' > data_silver.csv
 
 # Remove the page sources.
 rm page_gold page_silver
